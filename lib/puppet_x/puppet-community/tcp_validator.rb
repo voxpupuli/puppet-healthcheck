2ab12ff65c3588cf788333a31126ff991586776a
Tim Meusel <tim@bastelfreak.de>
2016-08-08 14:14:29 +0200

    rubocop: fix Style/CommentIndentation

--- lib/puppet_x/puppet-community/tcp_validator.rb
+++ lib/puppet_x/puppet-community/tcp_validator.rb
@@ -9,11 +9,11 @@ module PuppetX
       attr_reader :tcp_port
 
       def initialize(tcp_resource_name, tcp_server, tcp_port)
-          # NOTE (spredzy) : By relying on the uri module
-          # we rely on its well tested interface to parse
-          # both IPv4 and IPv6 based URL with a port specified.
-          # Unfortunately URI needs a scheme, hence the http
-          # string here to make the string URI compliant.
+        # NOTE (spredzy) : By relying on the uri module
+        # we rely on its well tested interface to parse
+        # both IPv4 and IPv6 based URL with a port specified.
+        # Unfortunately URI needs a scheme, hence the http
+        # string here to make the string URI compliant.
         uri = URI("http://#{tcp_resource_name}")
         @tcp_server = IPAddr.new(Socket.getaddrinfo(uri.host, nil)[0][3]).to_s
         @tcp_port = uri.port
