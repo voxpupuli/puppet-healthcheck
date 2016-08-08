888e068473f63e185bc0d7ba29ac7c8cfc6b67bc
Tim Meusel <tim@bastelfreak.de>
2016-08-08 14:14:05 +0200

    rubocop: fix Style/IndentationWidth

--- lib/puppet_x/puppet-community/tcp_validator.rb
+++ lib/puppet_x/puppet-community/tcp_validator.rb
@@ -14,12 +14,12 @@ module PuppetX
           # both IPv4 and IPv6 based URL with a port specified.
           # Unfortunately URI needs a scheme, hence the http
           # string here to make the string URI compliant.
-          uri = URI("http://#{tcp_resource_name}")
-          @tcp_server = IPAddr.new(Socket.getaddrinfo(uri.host, nil)[0][3]).to_s
-          @tcp_port = uri.port
-        rescue
-          @tcp_server = IPAddr.new(Socket.getaddrinfo(tcp_server, nil)[0][3]).to_s
-          @tcp_port   = tcp_port
+        uri = URI("http://#{tcp_resource_name}")
+        @tcp_server = IPAddr.new(Socket.getaddrinfo(uri.host, nil)[0][3]).to_s
+        @tcp_port = uri.port
+      rescue
+        @tcp_server = IPAddr.new(Socket.getaddrinfo(tcp_server, nil)[0][3]).to_s
+        @tcp_port   = tcp_port
       end
 
       # Utility method; attempts to make a tcp connection to the specified server.
